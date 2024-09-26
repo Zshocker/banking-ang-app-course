eb248b29335f22cc061ed2b0ef5fb5f349d4a2c3
Zshocker <senhicham0@gmail.com>
2024-09-26 21:28:00 +0100

    first commit

--- src/app/components/transfer/transfer.component.ts
+++ src/app/components/transfer/transfer.component.ts
@@ -1,15 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Account, Client} from "../../models/models";
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
   styleUrls: ['./transfer.component.css']
 })
-export class TransferComponent {
-  amount : number = 0;
+export class TransferComponent implements OnInit {
+
+  amount: number = 0;
   label = '';
   amountError = '';
   labelError = '';
+  currentUser?: Client;
+  account?: Account;
+
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('currentUser');
+    const storedAccountId = localStorage.getItem('storedAccountId');
+    if (storedUser) {
+      this.currentUser = JSON.parse(storedUser) as Client;
+      if (storedAccountId) {
+        this.account = this.currentUser.accounts.filter(s => s.id === parseInt(storedAccountId))[0];
+      }
+    }
+  }
 
   submitTransfer(): void {
     this.amountError = '';
@@ -26,17 +41,26 @@ export class TransferComponent {
       valid = false;
     }
 
-    if(valid)
+    if (valid && !this.accountVerify()) {
+      this.amountError = `Le montant doit etre inférieur ou égal à ${this.account?.balance}€.`;
+      valid = false;
+    }
+
+    if (valid)
       alert('Transfer in progress...');
   }
 
   validateAmount(): boolean {
     const numericValue = this.amount;
-    return !isNaN(numericValue) && numericValue >= 1 && numericValue <= 5000;
+    return !isNaN(numericValue) && numericValue >= 1 && numericValue <= 5500;
   }
 
   validateLabel(): boolean {
     const regex = /^[A-Za-z0-9\s]{0,50}$/;
     return regex.test(this.label);
+  }
+
+  private accountVerify() {
+    return this.amount <= (this.account?.balance ?? 0);
   }
 }
